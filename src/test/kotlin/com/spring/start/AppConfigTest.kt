7a44140884b3d2b14d73a8de4d4e3c6cfb667ee1
david.g <david.g@kakaopaysec.com>
2023-08-01 13:15:56 +0900

    AppConfigTest 수정

--- src/test/kotlin/com/spring/start/AppConfigTest.kt
+++ src/test/kotlin/com/spring/start/AppConfigTest.kt
@@ -12,6 +12,7 @@ class AppConfigTest {
         val member = Member(1L, "memberA", Grade.VIP)
         memberService.join(member)
 
+
         val findMember = memberService.findMember(1L)
         println("new member = ${member.name}")
         println("find member = ${findMember!!.name}")

5d681b8cab742811fafe59bf8b0e5665c67aa3f7
givxq <coooeeeeep@gmail.com>
2023-08-02 07:05:27 +0900

    ApplicationContextSameBeanFindTest 추가

---
+++ src/test/kotlin/com/spring/start/ApplicationContextSameBeanFindTest.kt
@@ -0,0 +1,40 @@
+package com.spring.start
+
+import com.spring.start.member.MemberRepository
+import com.spring.start.member.MemoryMemberRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+class ApplicationContextSameBeanFindTest {
+    private val ac = AnnotationConfigApplicationContext(SameBeanConfig::class.java)
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
+    fun findBeanByTypeDuplicate() {
+        assertThrows(NoUniqueBeanDefinitionException::class.java) {
+            ac.getBean(MemberRepository::class.java)
+        }
+    }
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
+    fun findBeanByName() {
+        val memberRepository = ac.getBean("memberRepository1", MemberRepository::class.java)
+        assertThat(memberRepository).isInstanceOf(MemberRepository::class.java)
+    }
+
+    @Configuration
+    internal class SameBeanConfig {
+        @Bean
+        fun memberRepository1() = MemoryMemberRepository()
+
+        @Bean
+        fun memberRepository2() = MemoryMemberRepository()
+    }
+}

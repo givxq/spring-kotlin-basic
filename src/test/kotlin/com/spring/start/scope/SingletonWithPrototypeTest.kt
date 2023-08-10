3d810a2032ac5cc5542b3cadb7132f8183a4c4c2
david.g <david.g@kakaopaysec.com>
2023-08-10 17:23:20 +0900

    add Singleton With Prototype Test

---
+++ src/test/kotlin/com/spring/start/scope/SingletonWithPrototypeTest.kt
@@ -0,0 +1,55 @@
+package com.spring.start.scope
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Scope
+
+class SingletonWithPrototypeTest : ShouldSpec({
+
+    should("prototype find") {
+        val ac = AnnotationConfigApplicationContext(PrototypeBean::class.java)
+
+        listOf(
+            ac.getBean(PrototypeBean::class.java), ac.getBean(PrototypeBean::class.java)
+        ).forEach {
+            it.addCount()
+            it.count shouldBe 1
+        }
+    }
+
+    should("singleton client use prototype") {
+        val ac = AnnotationConfigApplicationContext(ClientBean::class.java, PrototypeBean::class.java)
+        var count = 0
+
+        listOf(
+           ac.getBean(ClientBean::class.java), ac.getBean(ClientBean::class.java)
+        ).forEach {
+            it.logic() shouldBe ++count
+        }
+    }
+}) {
+    @Scope
+    internal class ClientBean(private val prototypeBean: PrototypeBean) {
+        fun logic(): Int {
+            prototypeBean.addCount()
+            return prototypeBean.count
+        }
+    }
+
+    @Scope("prototype")
+    internal class PrototypeBean {
+        var count = 0
+            private set
+
+        fun addCount() = count++
+
+        @PostConstruct
+        fun init() = println("PrototypeBean.init $this")
+
+        @PreDestroy
+        fun close() = println("PrototypeBean.close $this")
+    }
+}

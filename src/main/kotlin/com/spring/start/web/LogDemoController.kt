da31bf9d6e045c0e7320c33c856eead586d9961e
david.g <david.g@kakaopaysec.com>
2023-08-10 21:28:41 +0900

    add proxyMode = ScopedProxyMode.TARGET_CLASS

--- src/main/kotlin/com/spring/start/web/LogDemoController.kt
+++ src/main/kotlin/com/spring/start/web/LogDemoController.kt
@@ -2,7 +2,6 @@ package com.spring.start.web
 
 import com.spring.start.common.MyLogger
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -10,15 +9,15 @@ import org.springframework.web.bind.annotation.ResponseBody
 @Controller
 class LogDemoController(
     val logDemoService: LogDemoService,
-    val myLoggerProvider: ObjectProvider<MyLogger>
+    val myLogger: MyLogger
 ) {
     @RequestMapping("log-demo")
     @ResponseBody
     fun logDemo(request: HttpServletRequest): String {
         val requestURL = request.requestURL.toString()
-        val myLogger = myLoggerProvider.getObject()
         myLogger.requestURL = requestURL
 
+        println("+++ ${myLogger.javaClass}")
         myLogger.log("controller test")
         logDemoService.logic("testId")
         return "OK"
--- src/main/kotlin/com/spring/start/web/LogDemoService.kt
+++ src/main/kotlin/com/spring/start/web/LogDemoService.kt
@@ -1,12 +1,11 @@
 package com.spring.start.web
 
 import com.spring.start.common.MyLogger
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.stereotype.Service
 
 @Service
 class LogDemoService(
-    private val myLogger: ObjectProvider<MyLogger>
+    private val myLogger: MyLogger
 ) {
-    fun logic(id: String) = myLogger.getObject().log("service id = $id")
+    fun logic(id: String) = myLogger.log("service id = $id")
 }
